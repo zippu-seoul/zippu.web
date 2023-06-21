b956b2b3fcc323d82db9955b6c4e33de60a794c3
Seokhwan Won <hwani163@naver.com>
2023-06-21 22:10:06 +0900

    Add: add router

---
+++ src/pages/result/index.tsx
@@ -0,0 +1,34 @@
+import Head from 'next/head';
+import { NextPageContext } from 'next/types';
+export const getServerSideProps = async function ({
+  req,
+  query,
+}: NextPageContext) {
+  return {
+    props: {
+      title: query?.title || '',
+      description: query?.description || '',
+      tags: query?.tags || '',
+    },
+  };
+};
+
+export default function InviteAppointment(props: any) {
+  return (
+    <div style={{ display: 'flex', flex: 1 }}>
+      <img src="/ogimage.png" width={'100%'}></img>
+      <Head>
+        <title>{props.title}</title>
+        <meta name="description" content={props.description} />
+        <meta name="keywords" content={props.tags} />
+
+
+        <meta property="og:title" content={props.title} />
+        <meta property="og:image" content="/ogimage.png" />
+        <meta property="og:description" content={props.description} />
+        <meta property="og:url" content="Canonical link preview URL"></meta>
+      </Head>
+      {/* <div>앱에서 확인해 주세요</div> */}
+    </div>
+  );
+}
